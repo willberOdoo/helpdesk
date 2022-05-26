7a7774a2fddd2b56bb50de48f0d59b1b5a60962b
“ <willrr.payall.2021@gmail.com>
2022-05-26 11:41:13 -0400

    [ADD] folder init.py and manifest.py configuration

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Helpdesk',
+
+    'summary': """custom helpdesk app """,
+
+    'description': """ 
+      custom insider control module
+    """,
+    'autor': 'GiGaSOFT TECH',
+
+    'developer': 'Willber R.R',
+
+    'website': 'htps://www.odoo.com',
+
+    'category': 'Training',
+    'version': '0.1',
+
+    'depends': ['helpdesk'],
+
+    'data': [],
+
+    'demo': [],
+}
